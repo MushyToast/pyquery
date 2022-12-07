d1db533bdab992bc5d13474ccb86be3b22b66691
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-12-07 12:55:29 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
 
+setup(
+    name = 'pyquery',
+    version='Alpha 1.0',
+
+    url='https://github.com/MushyToast/pyquery',
+    author='mushytoast',
+    author_email='ndg72609@gmail.com',
+
+    py_modules=['pyquery'],
+)
